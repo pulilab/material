b098690c0277865265259ed0a459021572cda511
lordfuoco <lordfuoco@gmail.com>
2017-04-04 10:52:10 +0200

    explicit added dist folder to the repo and index to point to it, for npm  packaging

---
+++ dist/docs/js/build-config.js
@@ -0,0 +1,7 @@
+angular.module('docsApp').constant('BUILDCONFIG', {
+  "ngVersion": "1.5.5",
+  "version": "1.1.0",
+  "repository": "https://github.com/angular/material",
+  "commit": "79964dc9d8f65d3ca066b696c8425167575d8240",
+  "date": "2016-08-17 10:28:16 -0700"
+});
